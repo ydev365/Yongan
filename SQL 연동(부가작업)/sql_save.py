368e54407dc69dafd09630443d2845e4e848be65
undefinded <84854721+junsoo321@users.noreply.github.com>
2024-05-30 19:19:31 +0900

    Update sql_save.py

--- SQL 연동(부가작업)/sql_save.py
+++ SQL 연동(부가작업)/sql_save.py
@@ -14,11 +14,17 @@ folder_path = "C:/Users/Desktop 처럼 직접 수정 필요!!!!!!"
 
 files = os.listdir(folder_path)
 
-for file_name in files:
-    insert_query = "INSERT INTO users (name, image_path) VALUES (%s, %s)"
-    cursor.execute(insert_query, (file_name, os.path.join(folder_path, file_name)))
+for user_folder in os.listdir(folder_path):
+    user_name = user_folder.split("_")[1]  #폴더 이름의 _부분 이후에서 사용자 이름 추출
+    user_folder_path = os.path.join(folder_path, user_folder)
+    for file_name in os.listdir(user_folder_path):
+        image_path = os.path.join(user_folder_path, file_name)  # 이미지 파일 경로 생성
+        # 데이터베이스에 사용자 정보 저장
+        insert_query = "INSERT INTO users (name, image_path) VALUES (%s, %s)"
+        cursor.execute(insert_query, (user_name, image_path))
 
+#저장
 conn.commit()
-
+#종료
 cursor.close()
 conn.close()
