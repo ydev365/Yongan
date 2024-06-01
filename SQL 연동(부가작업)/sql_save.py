ec858eaefa25f338cfd48f07525768c0f385792e
undefinded <84854721+junsoo321@users.noreply.github.com>
2024-06-01 13:44:20 +0900

    Update sql_save.py

--- SQL 연동(부가작업)/sql_save.py
+++ SQL 연동(부가작업)/sql_save.py
@@ -10,21 +10,23 @@ conn = mysql.connector.connect( #이부분은 mysql서버와 연결하는 부분
 cursor = conn.cursor()
 
 #사진 데이터 저장된 경로
-folder_path = "C:/Users/Desktop 처럼 직접 수정 필요!!!!!!"
+f_path = "C:/Users/Desktop 처럼 직접 수정 필요!!!!!!"
 
-files = os.listdir(folder_path)
+def datebase_store_code(current_folder): #폴더를 탐색하여 데이터를 추출하는 함수 정의
+    for item in os.listdir(current_folder): #current_folder 경로의 모든 데이터를 나열
+        item_path = os.path.join(current_folder, item) #데이터별 경로 생성
+        if os.path.isdir(item_path): #current_folder 경로의 데이터가 폴더인지 확인
+            datebase_store_code(item_path) #폴더일경우 재귀함수로 폴더 내 데이터 추출
+        else: #폴더가 아닐경우 = 사진일경우
+            if item.endswith(('.jpg')):  # .jpg파일 필터링(.DS.Store 파일 등 걸러내는 용도)
+                user_name = item.split("_")[1]  # 첫번째 _부터 두번째 _사이의 사용자 이름 추출
+                insert_query = "INSERT INTO users (name, image_path) VALUES (%s, %s)" #SQL 쿼리 정의
+                cursor.execute(insert_query, (user_name, item_path)) #데이터베이스에 넣기
 
-for user_folder in os.listdir(folder_path):
-    user_name = user_folder.split("_")[1]  #폴더 이름의 _부분 이후에서 사용자 이름 추출
-    user_folder_path = os.path.join(folder_path, user_folder)
-    for file_name in os.listdir(user_folder_path):
-        image_path = os.path.join(user_folder_path, file_name)  # 이미지 파일 경로 생성
-        # 데이터베이스에 사용자 정보 저장
-        insert_query = "INSERT INTO users (name, image_path) VALUES (%s, %s)"
-        cursor.execute(insert_query, (user_name, image_path))
+datebase_store_code(f_path) #위에서 정의한 함수로 (f_path)부터 탐색 시작
 
-#저장
-conn.commit()
-#종료
+conn.commit()# 변경 사항 저장
+
+#연결 종료
 cursor.close()
 conn.close()
